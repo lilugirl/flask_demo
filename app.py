8293c0f1f911c89bddd250e1282ebcf0469f1cba
liu yi <lilugirl2005@126.com>
2021-10-31 02:37:15 +0800

    编辑post

--- app.py
+++ app.py
@@ -49,6 +49,19 @@ def delete(id):
     return redirect('/posts')
 
 
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    post = BlogPost.query.get_or_404(id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=post)
+
+
 @app.route('/home/users/<string:name>/posts/<int:id>')
 def hello(name, id):
     return "Hello, " + name + ",your id is "+str(id)
